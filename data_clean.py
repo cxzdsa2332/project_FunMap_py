b67472e8baedd77a855ac2671d015cd64c2e893b
ddaa <56618859+cxzdsa2332@users.noreply.github.com>
2021-06-29 13:14:09 +0800

    Create data_clean.py

---
+++ data_clean.py
@@ -0,0 +1,28 @@
+#import sys
+#sys.modules[__name__].__dict__.clear()
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+df_geno = pd.read_csv('geno_df.csv')
+df_ck = pd.read_csv('ck_df.csv',index_col=0)
+df_salt = pd.read_csv('salt_df.csv',index_col=0)
+
+#select sum_root
+df_ck = df_ck.loc[df_ck['roottype']=='sum_root'].iloc[:,1:15]
+df_salt = df_salt[df_salt['roottype']=='sum_root'].iloc[:,1:15]
+
+#get_mean for duplication
+df_ck = df_ck.groupby(df_ck.index).mean()
+df_salt = df_salt.groupby(df_salt.index).mean()
+
+#clean data
+df_pheno = pd.merge(df_ck,df_salt,left_index=True,right_index=True)
+df_geno = df_geno.loc[:,map(lambda x:str(x),df_pheno.index.to_list())]
+
+#biFunMap
+i=2
+df_pheno0 = df_pheno.loc[map(lambda x:int(x),df_geno.iloc[i,:][df_geno.iloc[i,:]==0].index)]
+df_pheno1 = df_pheno.loc[map(lambda x:int(x),df_geno.iloc[i,:][df_geno.iloc[i,:]==1].index)]
+df_pheno2 = df_pheno.loc[map(lambda x:int(x),df_geno.iloc[i,:][df_geno.iloc[i,:]==2].index)]
+print(df_pheno0)
